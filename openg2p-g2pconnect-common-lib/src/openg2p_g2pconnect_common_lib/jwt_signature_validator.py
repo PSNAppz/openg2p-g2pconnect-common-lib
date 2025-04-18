7b50fd67aff546bfb83bd3e2e05b19ef5ff2e8df
PSNAppZ <thepsnarayanan@gmail.com>
2025-04-18 11:38:30 +0530

    Remove logging of payload, headers, and URL in JWTSignatureValidator before sending verification request

--- openg2p-g2pconnect-common-lib/src/openg2p_g2pconnect_common_lib/jwt_signature_validator.py
+++ openg2p-g2pconnect-common-lib/src/openg2p_g2pconnect_common_lib/jwt_signature_validator.py
@@ -69,10 +69,7 @@ class JWTSignatureValidator(HTTPBearer):
                 "domain": str(DomainEnum.AUTH),
             },
         }
-
-        _logger.info(
-            f"Payload: {payload}, Headers: {headers}, URL: {_config.jwt_verify_url}"
-        )
+        # Send request to external service for verification
         async with httpx.AsyncClient() as client:
             response = await client.post(
                 _config.jwt_verify_url,
