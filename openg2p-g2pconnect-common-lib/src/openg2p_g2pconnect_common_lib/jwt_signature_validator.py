e3a53af2e4960fce48f6a699f1d0521a8a565126
PSNAppZ <thepsnarayanan@gmail.com>
2025-04-21 14:26:21 +0530

    Enhance logging to include verification URL in JWT response logging

--- openg2p-g2pconnect-common-lib/src/openg2p_g2pconnect_common_lib/jwt_signature_validator.py
+++ openg2p-g2pconnect-common-lib/src/openg2p_g2pconnect_common_lib/jwt_signature_validator.py
@@ -79,7 +79,7 @@ class JWTSignatureValidator(HTTPBearer):
                 headers=headers,
             )
             try:
-                _logger.info(f"Response: {response.json()}")
+                _logger.info(f"Response from {_config.jwt_verify_url} is: {response.json()}")
                 return response.json()["response"]["signatureValid"]
             except Exception as e:
                 _logger.error(f"Error: {e}")
