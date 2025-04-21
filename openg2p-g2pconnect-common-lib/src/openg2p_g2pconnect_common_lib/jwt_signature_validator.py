b1a4789a7ea0b888e9c55748ef526e1c5cdc25e1
PSNAppZ <thepsnarayanan@gmail.com>
2025-04-21 14:06:16 +0530

    Change payload logging level from debug to info in JWTSignatureValidator

--- openg2p-g2pconnect-common-lib/src/openg2p_g2pconnect_common_lib/jwt_signature_validator.py
+++ openg2p-g2pconnect-common-lib/src/openg2p_g2pconnect_common_lib/jwt_signature_validator.py
@@ -70,7 +70,7 @@ class JWTSignatureValidator(HTTPBearer):
                 "domain": str(DomainEnum.AUTH),
             },
         }
-        _logger.debug(f"Payload: {payload}")
+        _logger.info(f"Payload: {payload}")
         # Send request to external service for verification
         async with httpx.AsyncClient() as client:
             response = await client.post(
