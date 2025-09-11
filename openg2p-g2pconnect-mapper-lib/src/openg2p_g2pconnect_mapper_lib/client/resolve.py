5bcc9f250c71a197e9084cb6315d5bcf47e3688f
PSNAppZ <thepsnarayanan@gmail.com>
2025-09-11 15:26:06 +0530

    remove logger

--- openg2p-g2pconnect-mapper-lib/src/openg2p_g2pconnect_mapper_lib/client/resolve.py
+++ openg2p-g2pconnect-mapper-lib/src/openg2p_g2pconnect_mapper_lib/client/resolve.py
@@ -44,9 +44,6 @@ class MapperResolveClient(BaseService):
                 orig_headers["Signature"] = await self.crypto_helper.create_jwt_token(payload)
             if headers:
                 orig_headers.update(headers)
-            _logger.info(
-                f"Content to be sent to mapper resolve client: {orjson.dumps(payload, option=orjson.OPT_SORT_KEYS)}"
-            )
             res = await self.http_client.post(
                 self.url,
                 content=orjson.dumps(payload, option=orjson.OPT_SORT_KEYS),
